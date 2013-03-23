a8df2a9eea5a934b1afe5c87ee4e52686d616fe6
Philip Pham <ppham@sessionm.com>
2013-03-23 13:01:11 -0400

    fixed Mlogistic

--- R/Mlogistic.R
+++ R/Mlogistic.R
@@ -1,16 +1,26 @@
+
 Mlogistic <-
-function(y,fit,cov){
+  function(y,fit,cov) {
 	n <- length(y)
 	X <- model.matrix(fit)
 	MLE <- as.vector(fit$coef)
 	pi <- exp(MLE%*%t(X))/(1+exp(MLE%*%t(X)))
 	max_loglik<-log(pi)%*%y + log(1-pi)%*%(1-y)
 	input <- list(y=y,X=X,MLE=MLE,max_loglik=max_loglik)
-	H <- function(epsilon,input,VZ,cstar){
-		t(input$y) %*% t(log(exp(t(input$MLE+epsilon*VZ)%*%t(input$X))/(1+exp(t(input$MLE+epsilon*VZ)%*%t(input$X)))))+t(1-input$y) %*% t(log(1/(1+exp(t(input$MLE+epsilon*VZ)%*%t(input$X)))))-input$max_loglik+0.5*cstar}
-	H.lik <- function(y,X,MLE){t(y) %*% t(log(exp(MLE%*%t(X))/(1+exp(MLE%*%t(X)))))+t(1-y) %*% t(log(1/(1+exp(MLE%*%t(X)))))}
-  if (is.null(cov)){
-		cov <- vcov(fit)
-		list(cov=cov,input=input,H=H,H.lik=H.lik)}
-	else {list(input=input,H=H,H.lik=H.lik)}}
-
+	H <- function(epsilon,input,VZ,cstar) {
+      tmp <- exp(input$X %*% (input$MLE+epsilon*VZ))
+      input$y %*% log(tmp/(1+tmp)) + (1-input$y) %*% log(1/(1+tmp)) -
+        input$max_loglik+0.5*cstar
+    }
+	H.lik <- function(y,X,MLE) {
+      tmp <- exp(X %*% MLE)
+      y %*% log(tmp/(1+tmp)) + (1-y) %*% log(1/(1+tmp))
+    }
+    if (is.null(cov)) {
+      cov <- vcov(fit)
+      list(cov=cov,input=input,H=H,H.lik=H.lik)
+    }
+	else {
+      list(input=input,H=H,H.lik=H.lik)
+    }
+  }
